7fb164855a477922a6027a0c71832fa205c2d071
xttebrocx <samueldcorbett@uchicago.edu>
2022-05-19 14:19:50 +0000

    Just defining routes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,6 @@
 class ApplicationController < ActionController::Base
+
+  def homepage
+  render({:template=>"administrative/homepage.html.erb"})
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,15 @@
 Rails.application.routes.draw do
 
+get("/", :controller=>"application", :action=>"homepage")
 
+get("/user")
+
+get("/create_game") #will end on the game template 
+
+get("/join_game") #will render the game template
+
+get("/leave_game") #will not render a template, just perform action
+
+get("/game") #will render the game page
 
 end
